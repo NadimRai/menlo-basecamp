420bf2d4ad3b824c72f2a3f464a0662f0197989d
NadimRai <rai_nadim@yahoo.com>
2017-09-24 01:16:42 +0800

    created form for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'project/index'
+  get 'projects', to: 'projects#index'
+
+  post 'projects', to: 'projects#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
